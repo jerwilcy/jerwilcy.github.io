21d6297edbee83fad15866ce07426e4d747ac4ce
jerwilcy <cy_ncepu@163.com>
2024-12-04 18:06:06 +0800

    Site updated: 2024-12-04 18:06:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/04/新的博客文章/","2024/12/04/cy的博文/","2024/12/04/hello-world/"];function toRandomPost(){
+var posts=["chengyi/cy的博文/","chengyi/新的博客文章/","chengyi/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
